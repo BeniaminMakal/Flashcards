8a0241335a7b9edb39c8e1b97554046c32e231b2
Beniamin Makal <beniamin.makal@gmail.com>
2017-05-04 21:18:12 +0200

    Add form "AddCollection".

---
+++ Flashcards/Flashcards/AddCollection.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Flashcards
+{
+    partial class AddCollection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Add = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Collection_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(33, 101);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(33, 67);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 1;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Collection_name
+            // 
+            this.Collection_name.Location = new System.Drawing.Point(12, 30);
+            this.Collection_name.Name = "Collection_name";
+            this.Collection_name.Size = new System.Drawing.Size(121, 20);
+            this.Collection_name.TabIndex = 2;
+            this.Collection_name.TextChanged += new System.EventHandler(this.CollectionName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Collection name";
+            // 
+            // AddCollection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(157, 143);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Collection_name);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Add);
+            this.Name = "AddCollection";
+            this.Text = "AddCollection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.TextBox Collection_name;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Flashcards/Flashcards/AddCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flashcards
+{
+    public partial class AddCollection : Form
+    {
+        string collectionName { get; set; }
+        string foreignLanguage { get; set; }
+        string translationLanguage { get; set; }
+        int result { get; set; }
+    
+        public AddCollection()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void CollectionName_TextChanged(object sender, EventArgs e)
+        {
+            collectionName = Collection_name.Text;
+        }
+
+        private void Foreign_language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Translation_language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+
+                    this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
